03fc14ea436757c2cd013c204e1caffa6456e741
lejandro83 <lejandro83@gmail.com>
2017-01-21 00:16:55 -0600

    Adds dribbble js

---
+++ js/lagom.js
@@ -0,0 +1,17 @@
+$.jribbble.setToken('8b42722940fbcff4c3d40d20bf710ff275800e5e7f0ca354f83658aaeb1b620d');
+
+$.jribbble.users('lagggom').shots({per_page: 36}).then(function(shots) {
+  var html = [];
+  
+  shots.forEach(function(shot) {
+    html.push('<li class="shots--shot">');
+    html.push('<a href="' + shot.html_url + '" target="_blank">');
+    html.push('<img src="' + shot.images.normal + '">');
+    html.push('<h3>' + shot.title + '</h3>');
+    html.push('<p>' + shot.description + '</p>');
+    html.push('</a></li>');
+    // console.log(shot);
+  });
+  
+  $('.shots').html(html.join(''));
+});
